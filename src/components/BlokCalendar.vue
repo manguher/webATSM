d63f0711347edefb50043c7994f7e0fb16759feb
Manu <manuel.gutierrez@gat-blac.com>
2020-07-13 22:49:11 -0400

    lighbox images

--- src/components/BlokCalendar.vue
+++ src/components/BlokCalendar.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="col-md-2 card-event-content">
+    <CoolLightBox :items="items" :index="index" @close="index = null"></CoolLightBox>
     <div class="card body-calendar-detalle" style="width: 18rem;">
       <div class="card-block">
-        <img :src="blok.imagenUT.filename" class="card-img-top" alt />
+        <img :src="blok.imagenUT.filename" @click="index = 0" class="card-img-top" alt />
         <div class="card-body">
           <h5 class="card-title">
             <strong>{{blok.Title}}</strong>
@@ -24,6 +25,9 @@
   </div>
 </template>
 <script>
+import CoolLightBox from "vue-cool-lightbox";
+import "vue-cool-lightbox/dist/vue-cool-lightbox.min.css";
+
 export default {
   name: "BlokCalendar",
   props: {
@@ -31,6 +35,9 @@ export default {
       type: Object,
       required: true
     }
+  },
+  components: {
+    CoolLightBox
   },
   data() {
     return {
@@ -51,13 +58,16 @@ export default {
         "Octubre",
         "Noviembre",
         "Diciembre"
-      ]
+      ],
+       items: [],
+      index: null
     };
   },
   mounted() {
     this.dia = this.getDia();
     this.hora = this.getHora();
     this.mes = this.getMes();
+    this.items.push(this.blok.imagenUT.filename)
   },
   methods: {
     getHora() {
